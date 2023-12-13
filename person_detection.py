7d09ed03fec1ae80eb4f5bea7476b295f7c1d1bf
Siddharth <2004sid@gmail.com>
2023-12-13 11:32:41 +0530

    saved engaged detection video

--- person_detection.py
+++ person_detection.py
@@ -2,7 +2,8 @@ from ultralytics import YOLO
 import cv2
 
 def main():
-    model = YOLO('yolov8n.pt')
+    model = YOLO('runs/detect/train/weights/best.pt') # engaged detection
+    # model = YOLO('yolov8n.pt') # person detection
     frames=[]
 
     cap = cv2.VideoCapture('starter_images/vid1.mp4')
@@ -10,11 +11,11 @@ def main():
     while cap.isOpened():
         success, frame = cap.read()
         if success:
-            results = model.predict(frame, classes=[0], augment=False, show_conf=False, show_labels=False)
+            results = model.predict(frame, classes=[0], augment=False, show_conf=False, show_labels=False, conf=0.8)
             annotated_frame = results[0].plot()
             frames.append(annotated_frame)
 
-            # cv2.imshow("Person Detection", annotated_frame)
+            cv2.imshow("Person Detection", annotated_frame)
 
             if cv2.waitKey(1) and 0xFF == ord("q"):
                 break
@@ -25,17 +26,17 @@ def main():
     cv2.destroyAllWindows()
     return frames
 
-# frames = main()
+frames = main()
 
 # writing the annotated frames to a video
-# video_name = "output.mp4"
-# fps = 25
-# fourcc = cv2.VideoWriter_fourcc(*"mp4v")
-# frame_size = (960, 540)
+video_name = "output.mp4"
+fps = 25
+fourcc = cv2.VideoWriter_fourcc(*"mp4v")
+frame_size = (960, 540)
 
-# writer = cv2.VideoWriter(video_name, fourcc, fps, frame_size)
+writer = cv2.VideoWriter(video_name, fourcc, fps, frame_size)
 
-# for frame in frames:
-#     writer.write(frame)
+for frame in frames:
+    writer.write(frame)
 
-# writer.release()
+writer.release()
