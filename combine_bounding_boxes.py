78e277790fb30289b5cfd41e71606463cf71d6a8
Siddharth <2004sid@gmail.com>
2023-12-13 11:59:35 +0530

    bounding box coordinates

--- combine_bounding_boxes.py
+++ combine_bounding_boxes.py
@@ -8,7 +8,7 @@ bounding_boxes = []
 
 results = model.predict(['starter_images/img1.jpg', 'starter_images/img2.jpg', 'starter_images/img3.jpg'], classes=[0], show_conf=False, show_labels=False) 
 for i,r in enumerate(results):
-    print(r.boxes)
+    print(r.boxes.xyxy)
     im_arr = r.plot()
     im = Image.fromarray(im_arr[..., ::-1])
 
