808b3598f11df2281e2781e6e6d7b6a55d9da07d
Siddharth <2004sid@gmail.com>
2023-12-14 22:53:12 +0530

    merged vid clip

--- person_detection.py
+++ person_detection.py
@@ -8,27 +8,56 @@ def mergeable(box1, box2, x_val=100, y_val=100):
     (
         x1,
         y1,
-        w1,
-        h1,
-    ) = box1
-    (
         x2,
         y2,
-        w2,
-        h2,
+    ) = box1
+    (
+        x3,
+        y3,
+        x4,
+        y4,
     ) = box2
-    return (
-        max(x1, x2) - min(x1, x2) - minx_w(x1, w1, x2, w2) < x_val
-        and max(y1, y2) - min(y1, y2) - miny_h(y1, h1, y2, h2) < y_val
+
+    intersection_check = not (
+        (x3>x_val+x2 or x4+x_val<x1) or (y3>y2+y_val or y4+y_val<y1)
     )
 
 
+    w1 = x2-x1
+    w2 = x4-x3
+    h1 = y2-y1
+    h2 = y4-y3
+
+    dimension_check = not ( (w1<.5*w2 and h1<.5*h2) or (w2<.5*w1 and h2<.5*h1))
+
+    area1 = w1*h1
+    area2 = w2*h2
+
+    # area_check = not ( (area1<.5*area2) or (area2<.5*area1) )
+    
+    return intersection_check and dimension_check
+
+
 def merge(box1, box2):
-    x = min(box1[0], box2[0])
-    y = min(box1[1], box2[1])
-    w = max(box1[0] + box1[2], box2[0] + box2[2]) - x
-    h = max(box1[1] + box1[3], box2[1] + box2[3]) - y
-    return x, y, w, h
+    (
+        x1,
+        y1,
+        x2,
+        y2,
+    ) = box1
+    (
+        x3,
+        y3,
+        x4,
+        y4,
+    ) = box2
+
+    x_min = min(x1, x2, x3, x4)
+    y_min = min(y1, y2, y3, y4)
+    x_max = max(x1, x2, x3, x4)
+    y_max = max(y1, y2, y3, y4)
+
+    return x_min, y_min, x_max, y_max
 
 
 minx_w = lambda x1, w1, x2, w2: w1 if x1 <= x2 else w2
@@ -39,23 +68,19 @@ def draw_boxes(t, img, ind=''):
     if not t:
         return img
     for i in t:
-        x0 = i[0] - i[2] / 2
-        x1 = i[0] + i[2] / 2
-        y0 = i[1] - i[3] / 2
-        y1 = i[1] + i[3] / 2
-        start_point = (int(x0), int(y0))
-        end_point = (int(x1), int(y1))
+        (x1,y1,x2,y2,) = i
 
+        start_point = (int(x1), int(y1))
+        end_point = (int(x2), int(y2))
 
-        im_arr = cv2.rectangle(
-            img, start_point, end_point, color=(0, 0, 255), thickness=5
+        img = cv2.rectangle(
+            img, start_point, end_point, color=(0,0,255), thickness=5
         )
-        img = im_arr
 
     # cv2.imwrite(f'starter_images/merged{ind}.jpg', img)
-    # img = Image.fromarray(im_arr[..., ::-1])
-    # img.show()
-    return im_arr
+    # fin_img = Image.fromarray(im_arr[..., ::-1])
+    # fin_img.show()
+    return img
 
 def main():
     # model = YOLO('runs/detect/train/weights/best.pt') # engaged detection
@@ -73,8 +98,9 @@ def main():
             results = model.predict(frame, classes=[0], augment=False, show_conf=False, show_labels=False, conf=0.8)
             
             for r in results:
-                bounding_boxes.extend(r.boxes.xywh.tolist())
-                for j,k in combinations(r.boxes.xywh.tolist(), r=2):
+                bounding_boxes.extend(r.boxes.xyxy.tolist())
+
+                for j,k in combinations(r.boxes.xyxy.tolist(), r=2):
                     if mergeable(j,k):
                         t.append(list(merge(j,k)))
                         try:
