1baab06892d6bab698ba31225c0a340468142889
Siddharth <2004sid@gmail.com>
2023-12-19 16:14:15 +0530

    flask video set up

--- zooming_app.py
+++ zooming_app.py
@@ -1,7 +1,16 @@
 from flask import Flask, render_template, request
 import json
+import base64
+from io import BytesIO
+from PIL import Image
 
 app = Flask(__name__)
+
+def decode_save(base64_str):
+    binary_data = base64.b64decode(base64_str)
+    image_buffer = BytesIO(binary_data)
+    image = Image.open(image_buffer)
+
 
 @app.route('/')
 def home():
@@ -11,9 +20,9 @@ def home():
 def process():
     print('test')
     output = request.get_json()
-    print(output, type(output))
     result = output['value']
-    print(result, type(result))
+    # decode_save(result)
+    # print(result, type(result))
     return result
 
 if __name__=='__main__':
