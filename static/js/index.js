96831e03a372cea68ca097d81470167fdd001ed6
Siddharth <2004sid@gmail.com>
2023-12-27 14:17:19 +0530

    passing original video fps to js file on initial site load

--- static/js/index.js
+++ static/js/index.js
@@ -4,7 +4,7 @@ const frameNumberElement = document.getElementById("frameNumber");
 const canvas = document.createElement("canvas");
 const ctx = canvas.getContext("2d");
 
-// console.log(original_fps)
+console.log(original_fps)
 
 // play/pause button behaviour
 function play() {
